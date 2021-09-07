2741e0f9add4f86b4debf2f7ebec96d78db123d1
Tung170902 <70988911+Tung170902@users.noreply.github.com>
2021-09-08 00:11:01 +0700

    gg

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table("users")->insert([
+        "name"=>"Administrator",
+        "email"=>"admin@gmail.com",
+        "password"=>bcrypt("11111111"),
+    ]);
     }
 }
